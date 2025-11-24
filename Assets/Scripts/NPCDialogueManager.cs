3910ed19c36573465f83c822c5cb226527969d02
AniyahC <user@FIH-COM23a103.fsu.edu>
2025-11-24 17:51:11 -0500

    added animations and code

---
+++ Assets/Scripts/NPCDialogueManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class NPCDialogueManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public AudioClip loopingCall;
+
+    public bool playLoopAtStart = true;
+
+    public AudioClip[] lines;
+
+    public GameObject marker;
+
+    public ObjectiveManager objectiveManager;
+    public int objectiveIndexToChange = 1;
+    public string newObjectiveText;
+
+    private int currentIndex = -1;
+    private bool loopStopped = false;
+
+    void Start()
+    {
+        if (playLoopAtStart && loopingCall != null)
+        {
+            audioSource.clip = loopingCall;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+
+    public void Interact()
+    {
+        if (!loopStopped)
+        {
+            audioSource.Stop();
+            audioSource.loop = false;
+            loopStopped = true;
+        }
+
+        currentIndex++;
+
+        // Out of lines
+        if (currentIndex >= lines.Length)
+        {
+            if (marker != null) marker.SetActive(false);
+            return;
+        }
+
+        // Play next line
+        audioSource.clip = lines[currentIndex];
+        audioSource.Play();
+
+        // If this is the objective-changing line:
+        if (currentIndex == objectiveIndexToChange)
+        {
+            objectiveManager.SetObjective(newObjectiveText);
+        }
+    }
+}
